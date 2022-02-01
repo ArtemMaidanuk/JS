55b1166dfff667dfb008cbb1adb573eece86449b
ArtemMaidanuk <maidanukartem@gmail.com>
2022-02-01 14:38:43 +0200

    do hw and cw

---
+++ lesson1/class-work/main.js
@@ -0,0 +1,81 @@
+// Завдання1
+
+// let arr = [10,20,30,40,50,60,70,80,90,100];
+// let result = arr;
+// console.log (result)
+
+// Завдання2
+
+// let book = {
+//     name: 'Дракула (Мара)',
+//     page: '210'',
+//     genre: 'трейлер'
+// }
+
+// Завдання3
+
+// let book = {
+//     name: 'Пів короля',
+//     page: '296',
+//     genre: 'фентезі',
+//     authors: {
+//         authors1:'Джо Аберкромбі',
+//         authors2:'Тарас Копанський'
+//     }
+//
+// }
+
+// Завдання4
+
+// let arrBook = []
+//  arrBook[0] = {
+//      name: 'Пів короля',
+//      page: '296',
+//      genre: 'фентезі',
+//      authors: {
+//          authors1:'Джо Аберкромбі',
+//          authors2:'Тарас Копанський'
+//      }
+// }
+// arrBook[1] = {
+//     name: 'Рухомі картинки',
+//     page: '448',
+//     genre: 'фентезі',
+//     authors:'Террі Пратчетт'
+// }
+// arrBook[2] = {
+//     name: 'Піраміди',
+//     page: '376',
+//     genre: 'фентезі',
+//     authors:'Террі Пратчетт'
+// }
+// arrBook[3] = {
+//     name: 'ПДев’ятий Дім',
+//     page: '528296',
+//     genre: 'фентезі',
+//     authors:'Лі Бардуґо'
+// }
+//
+// console.log (arrBook)
+
+// Завдання5
+
+// let height = 23;
+// let width = 10;
+// let s = height*width;
+// console.log (s)
+
+// Завдання6
+
+// let heightC =10;
+// let dC = 4;
+// const Pi = 3.14
+// let v = heightC*Pi*dC;
+// console.log (v)
+
+// Завдання7
+
+// let n = 3;
+// let m = 4;
+// let k = Math.sqrt(n**2+m**2)
+// console.log (k)
---
+++ lesson1/home-work/main.js
@@ -0,0 +1,101 @@
+// Завдання1
+
+// let a = 'hello';
+// let b = 'owu';
+// let c = 'com';
+// let d = 'ua';
+// let e = 1;
+// let f = 10;
+// let g = -999;
+// let h = 123;
+// let i = 3.14;
+// let j = 2.7;
+// let k = 16;
+// let l = true;
+// let m = false;
+// console.log(a, b, c, d, e, f, g, h, i, j, k, l, m);
+// alert(a + b + c + d + e + f + g + h + i + j + k + l + m);
+// document.write (a + "-" + b + "-" + c + "-" + d
+// + "-" + e + "-" + f + "-" + g + "-" + h + "-" + i  + "-"
+//     + j + "-" + k + "-" + l  + "-" + m
+// );
+
+// Завдання2
+
+// let a = 'my';
+// let b = 'name';
+// let c = 'is';
+// let d = 'Artem';
+// let e = 21;
+// let f = 2001;
+// let g = 1999;
+// let h = 321;
+// let i = 314;
+// let j = 217;
+// let k = 160;
+// let l = false;
+// let m = true;
+// console.log(a, b, c, d, e, f, g, h, i, j, k, l, m);
+// alert(a + b + c + d + e + f + g + h + i + j + k + l + m);
+// document.write (a + "-" + b + "-" + c + "-" + d
+// + "-" + e + "-" + f + "-" + g + "-" + h + "-" + i  + "-"
+//     + j + "-" + k + "-" + l  + "-" + m
+// );
+// Завдання3
+
+// let firstName = 'Maidanuk';
+// let middleName = ' Victorovuch';
+// let lastName = ' Artem';
+// let person = firstName + lastName + middleName;
+// console.log(person);
+
+// Завдання4
+
+// let firstName = prompt('Як тебе звати');
+// let middleName = prompt('Як тебе по-батькові');
+// let age = prompt('скільки тобі років');
+// alert('Вітаю ' + firstName + " " + middleName+ "." +
+//     " Тобі " + age + " років.")
+
+// Завдання5
+
+// let a = 100;
+// let b = '100';
+// let c = true;
+// console.log (  typeof a)
+// console.log (  typeof b)
+// console.log (  typeof c)
+
+// Завдання6
+
+// let a = 5 < 6;
+// let b = 5 > 6;
+// let c = 5 === 6;
+// let d = 5 == 6;
+// let e = 10 === 10;
+// let f = 10 == 10;
+// let g = 10 != 10;
+// let h = 10 > 10;
+// let i = 10 < 10;
+// let j = 123 === '123';
+// let k = 123 !== '123';
+// console.log(a);
+// console.log( b);
+// console.log(c);
+// console.log(d;
+// console.log(e);
+// console.log(f);
+// console.log(g);
+// console.log(h);
+// console.log(i);
+// console.log(j);
+// console.log( k);
+
+// Завдання7
+
+// let str = "20";
+// let a = 5;
+// document.write(str + a + "<br/>"); при додаванні рядка і числа, js сприймаэ все як рядок а при відніманні ні
+// document.write(str - a + "<br/>");
+// document.write(str * "2" + "<br/>");
+// document.write(str / 2 + "<br/>");
