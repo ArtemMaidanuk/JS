daa99adfbc40154087416ac64d2e21baecf2df29
ArtemMaidanuk <maidanukartem@gmail.com>
2022-02-03 16:47:32 +0200

    do hw and cw

---
+++ lesson2/ad/main.js
@@ -0,0 +1,4 @@
+let a = +prompt('svscsc')
+(a>-100 && a<0 ) ? console.log ('негативним') : console.log ('       d') ;
+(a>0 && a<100 ) ? console.log ('позитивним') : console.log ('35423242') ;
+(a=0) ? console.log ('0') : console.log ('dsbsfvs') ;
